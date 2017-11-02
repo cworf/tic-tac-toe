86855763c5adc614e056d511f1f460575bb1bdb4
Colin Worf and John Sawiris <john@>
2017-11-02 11:10:33 -0700

    add objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
-var row1 = ["", "", ""];
-var row2 = ["", "", ""];
-var row3 = ["", "", ""];
+function Player(mark, active) {
+  this.mark = mark;
+}
+Player.prototype.mark = function(playerMark) {
+  return this.mark = playerMark;
+}
 
-var col1 = ["", "",""];
-var col2 = ["", "", ""];
-var col3 = ["", "", ""];
 
+
+//Front end
 $(function(){
+  var playerX = new Player("X", true);
+  var playerO = new Player("O", false);
+
+
   var nextChar = "x";
   $('.box').click(function() {
     if ($(this).text() === "") {
